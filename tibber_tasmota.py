ff4c7f9c879d30feae307081cdec21a7563bb07d
E-t0m <110770475+E-t0m@users.noreply.github.com>
2024-10-16 23:00:57 +0200

    Update tibber_tasmota.py

--- tibber_tasmota.py
+++ tibber_tasmota.py
@@ -37,22 +37,23 @@ def main():
 	price_min = min(future_prices.values())
 	price_max = max(future_prices.values())
 	price_spread = (price_max-price_min)*100
-	price_lt = price_avg - (price_spread / 6)		# set the divider to your needs
-	price_ut = price_avg + (price_spread / 10)		# it sets the threshold for the timers
+	price_lt = price_avg - (price_spread / 4)		# set the divider to your needs
+	price_ut = price_avg #- (price_spread /5 )		# it sets the threshold for the timers
 	
 	if verbose:	print('avg: %.2f'%price_avg,'min: %.2f'%(100*price_min),'max: %.2f'%(100*price_max),'spread: %.2f'%price_spread,'lt: %.2f'%price_lt,'ht: %.2f'%price_ut)
 	
 	tib_hour_now = datetime.now().strftime('%Y-%m-%dT%H')
 	timer_is_set = [False]*(len(tasmota_dev)+1) 	# ignore index 0
 	zerotimer = dict()
-	hot_sw_on = True								# True disables hot switching!
+	hot_on = True								# True disables hot switching!
+	hot_off = True
 	
 	for cur_p_time in prices:
 		
 		cur_price = prices[cur_p_time]*100
 		cur_timer = cur_p_time[-2:]+':00'
 		p_hour_is_cur = True if tib_hour_now == cur_p_time else False
-		msg = 'now' if tib_hour_now == cur_p_time else ' '
+		msg = 'now ' if tib_hour_now == cur_p_time else ' '
 		past_time = False if cur_p_time in future_prices else True
 		
 		if past_time:
@@ -65,35 +66,46 @@ def main():
 				msg = '  '+msg
 				
 				if p_hour_is_cur:
-					if hot_sw_on: pass 
+					if hot_on: pass 
 					else:
-						msg += 'hot sw on:'
-						hot_sw_on = True
+						msg += 'hot on:'
+						hot_on = True
 						msg += ' 1' if tasmota_switch( tasmota_dev['auto1_on'],'1').status_code == 200 else ' 1 error'
 						msg += ' 2' if tasmota_switch( tasmota_dev['auto2_on'],'1').status_code == 200 else ' 2 error'
 						if not verbose: syslog.syslog(syslog.LOG_INFO, msg)
 				else:
-					if not timer_is_set[1] and not timer_is_set[2] and not timer_is_set[3] and not timer_is_set[4]:
-						msg += 'T on set:'; timer_is_set[1] = True; timer_is_set[2] = True
+					if not timer_is_set[1] and not timer_is_set[2]:
+						msg += ' T on:'; timer_is_set[1] = True; timer_is_set[2] = True
 						msg += ' 1' if tasmota_timer( tasmota_dev['auto1_on'],cur_timer,'1').status_code == 200 else ' 1 error'
 						msg += ' 2' if tasmota_timer( tasmota_dev['auto2_on'],cur_timer,'1').status_code == 200 else ' 2 error'
 						if not verbose: syslog.syslog(syslog.LOG_INFO, msg +' at '+ cur_timer)
+				
 			
-			elif price_ut < cur_price:				# upper threshold
-				p_char = '<'
-				if not past_time:
-					msg = 'Z '+msg
-					zerotimer[cur_p_time] = cur_price	# current hour will be enabled in the timerfile
-			
-			else:									# middle
-				p_char = '-'
+			else:									# middle and upper
+				p_char = '|'
 				msg = '  '+msg
 				
-				if timer_is_set[1] and timer_is_set[2] and not timer_is_set[3] and not timer_is_set[4]:
-					msg += 'T off set:'; timer_is_set[3] = True; timer_is_set[4] = True
-					msg += ' 3' if tasmota_timer( tasmota_dev['auto1_off'],cur_timer,'0').status_code == 200 else ' 3 error'
-					msg += ' 4' if tasmota_timer( tasmota_dev['auto2_off'],cur_timer,'0').status_code == 200 else ' 4 error'
-					if not verbose: syslog.syslog(syslog.LOG_INFO, msg +' at '+ cur_timer)
+				if p_hour_is_cur:
+					if hot_off: pass 
+					else:
+						msg += 'hot off:'
+						hot_off = True
+						msg += ' 1' if tasmota_switch( tasmota_dev['auto1_off'],'0').status_code == 200 else ' 1 error'
+						msg += ' 2' if tasmota_switch( tasmota_dev['auto2_off'],'0').status_code == 200 else ' 2 error'
+						if not verbose: syslog.syslog(syslog.LOG_INFO, msg)
+				else:
+					if not timer_is_set[3] and not timer_is_set[4]:
+						msg += 'T off:'; timer_is_set[3] = True; timer_is_set[4] = True
+						msg += ' 3' if tasmota_timer( tasmota_dev['auto1_off'],cur_timer,'0').status_code == 200 else ' 3 error'
+						msg += ' 4' if tasmota_timer( tasmota_dev['auto2_off'],cur_timer,'0').status_code == 200 else ' 4 error'
+						if not verbose: syslog.syslog(syslog.LOG_INFO, msg +' at '+ cur_timer)
+				
+				if price_ut < cur_price:			# upper threshold
+					p_char = '<'
+					if not past_time:
+						msg = 'Z '+msg
+						zerotimer[cur_p_time] = cur_price	# current hour will be enabled in the timerfile
+			
 		
 		if verbose: print(str(msg).ljust(20),cur_p_time,'%2.2f'%cur_price,str(p_char).rjust(int(cur_price)))
 	
@@ -103,9 +115,9 @@ def main():
 		with open('/home/vzlogger/timer.txt','w') as fo:
 			fo.write('# 0000-00-00 for daily repeating, space or tab separated\n#                   battery discharge W if > 100, percentage if <= 100\n# date     time     |   ac inverter power W if > 100, percentage if <= 100\n') #fileheader
 			for tib_form in future_prices:
-				file_form = tib_form.replace('T',' ') + ':00:00 ' + ('100 100' if tib_form in zerotimer else '0 0')
+				file_form = tib_form.replace('T',' ') + ':00:00 ' + ('1200 1200' if tib_form in zerotimer else '0 0')
 				fo.write(file_form+'\n')
-				if verbose: print(file_form)
+				#if verbose: print(file_form)
 	
 	return(0)
 
