b4a1b6eff8b04cd5cc2472fa7310e5edd041a35c
E-t0m <110770475+E-t0m@users.noreply.github.com>
2024-10-18 07:33:16 +0200

    Update tibber_tasmota.py

--- tibber_tasmota.py
+++ tibber_tasmota.py
@@ -15,10 +15,18 @@ tasmota_dev =	{	'auto1_on':{'ip':'192.168.178.88','timer_id': '1','output':'1'},
 verbose = True if '-v' in argv else False
 
 def tasmota_timer(dev,time,action):
-	return( get('http://'+dev['ip']+'/cm?cmnd=Timer'+dev['timer_id']+'{\"Enable\":1,\"Mode\":0,\"Time\":\"'+time+'\",\"Window\":\"0\",\"Days\":\"SMTWTFS\",\"Repeat\":0,\"Output\":'+dev['output']+',\"Action\":'+action+'}' ) )
+	try:
+		res = get('http://'+dev['ip']+'/cm?cmnd=Timer'+dev['timer_id']+'{\"Enable\":1,\"Mode\":0,\"Time\":\"'+time+'\",\"Window\":\"0\",\"Days\":\"SMTWTFS\",\"Repeat\":0,\"Output\":'+dev['output']+',\"Action\":'+action+'}' ).status_code
+	except:
+		res = 1
+	return(res)
 
 def tasmota_switch(dev,action):
-	return( get('http://'+dev['ip']+'/cm?cmnd=Power'+dev['output']+'%20'+action ) )
+	try:
+		res = get('http://'+dev['ip']+'/cm?cmnd=Power'+dev['output']+'%20'+action ).status_code
+	except:
+		res = 1
+	return(res)
 
 def main():
 	prices = dict()
@@ -37,10 +45,13 @@ def main():
 	price_min = min(future_prices.values())
 	price_max = max(future_prices.values())
 	price_spread = (price_max-price_min)*100
-	price_lt = price_avg - (price_spread / 4)		# set the divider to your needs
-	price_ut = price_avg #- (price_spread /5 )		# it sets the threshold for the timers
+	price_lt = price_avg - (price_spread / 4 )		# set the divider to your needs
+	price_ut = price_avg #(price_spread / 15 )		# it sets the threshold for the timers
+	price_lt_max = 22								# in ¢, fixed maximum for timer activation
 	
 	if verbose:	print('avg: %.2f'%price_avg,'min: %.2f'%(100*price_min),'max: %.2f'%(100*price_max),'spread: %.2f'%price_spread,'lt: %.2f'%price_lt,'ht: %.2f'%price_ut)
+	if price_lt > price_lt_max: price_lt = price_lt_max
+	if verbose: print('set lt to max: %.2f'%price_lt)
 	
 	tib_hour_now = datetime.now().strftime('%Y-%m-%dT%H')
 	timer_is_set = [False]*(len(tasmota_dev)+1) 	# ignore index 0
@@ -61,7 +72,7 @@ def main():
 			p_char = 'o'
 		else:										# current and future hours
 			
-			if price_lt > cur_price: 				# lower threshold
+			if cur_price < price_lt: 				# lower threshold
 				p_char = '>'
 				msg = '  '+msg
 				
@@ -70,14 +81,14 @@ def main():
 					else:
 						msg += 'hot on:'
 						hot_on = True
-						msg += ' 1' if tasmota_switch( tasmota_dev['auto1_on'],'1').status_code == 200 else ' 1 error'
-						msg += ' 2' if tasmota_switch( tasmota_dev['auto2_on'],'1').status_code == 200 else ' 2 error'
+						msg += ' 1' if tasmota_switch( tasmota_dev['auto1_on'],'1') == 200 else ' 1FAIL'
+						msg += ' 2' if tasmota_switch( tasmota_dev['auto2_on'],'1') == 200 else ' 2FAIL'
 						if not verbose: syslog.syslog(syslog.LOG_INFO, msg)
 				else:
 					if not timer_is_set[1] and not timer_is_set[2]:
 						msg += ' T on:'; timer_is_set[1] = True; timer_is_set[2] = True
-						msg += ' 1' if tasmota_timer( tasmota_dev['auto1_on'],cur_timer,'1').status_code == 200 else ' 1 error'
-						msg += ' 2' if tasmota_timer( tasmota_dev['auto2_on'],cur_timer,'1').status_code == 200 else ' 2 error'
+						msg += ' 1' if tasmota_timer( tasmota_dev['auto1_on'],cur_timer,'1') == 200 else ' 1FAIL'
+						msg += ' 2' if tasmota_timer( tasmota_dev['auto2_on'],cur_timer,'1') == 200 else ' 2FAIL'
 						if not verbose: syslog.syslog(syslog.LOG_INFO, msg +' at '+ cur_timer)
 				
 			
@@ -90,14 +101,14 @@ def main():
 					else:
 						msg += 'hot off:'
 						hot_off = True
-						msg += ' 1' if tasmota_switch( tasmota_dev['auto1_off'],'0').status_code == 200 else ' 1 error'
-						msg += ' 2' if tasmota_switch( tasmota_dev['auto2_off'],'0').status_code == 200 else ' 2 error'
+						msg += ' 1' if tasmota_switch( tasmota_dev['auto1_off'],'0') == 200 else ' 1FAIL'
+						msg += ' 2' if tasmota_switch( tasmota_dev['auto2_off'],'0') == 200 else ' 2FAIL'
 						if not verbose: syslog.syslog(syslog.LOG_INFO, msg)
 				else:
 					if not timer_is_set[3] and not timer_is_set[4]:
 						msg += 'T off:'; timer_is_set[3] = True; timer_is_set[4] = True
-						msg += ' 3' if tasmota_timer( tasmota_dev['auto1_off'],cur_timer,'0').status_code == 200 else ' 3 error'
-						msg += ' 4' if tasmota_timer( tasmota_dev['auto2_off'],cur_timer,'0').status_code == 200 else ' 4 error'
+						msg += ' 3' if tasmota_timer( tasmota_dev['auto1_off'],cur_timer,'0') == 200 else ' 3FAIL'
+						msg += ' 4' if tasmota_timer( tasmota_dev['auto2_off'],cur_timer,'0') == 200 else ' 4FAIL'
 						if not verbose: syslog.syslog(syslog.LOG_INFO, msg +' at '+ cur_timer)
 				
 				if price_ut < cur_price:			# upper threshold
@@ -115,9 +126,9 @@ def main():
 		with open('/home/vzlogger/timer.txt','w') as fo:
 			fo.write('# 0000-00-00 for daily repeating, space or tab separated\n#                   battery discharge W if > 100, percentage if <= 100\n# date     time     |   ac inverter power W if > 100, percentage if <= 100\n') #fileheader
 			for tib_form in future_prices:
-				file_form = tib_form.replace('T',' ') + ':00:00 ' + ('1200 1200' if tib_form in zerotimer else '0 0')
+				file_form = tib_form.replace('T',' ') + ':00:00 ' + ('100 100' if tib_form in zerotimer else '0 0')
 				fo.write(file_form+'\n')
-				#if verbose: print(file_form)
+				if '-vt' in argv : print(file_form)		# console output with -vt
 	
 	return(0)
 
