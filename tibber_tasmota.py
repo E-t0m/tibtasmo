11ec722a7b8a0a1d98178d75037259c732d77f46
E-t0m <110770475+E-t0m@users.noreply.github.com>
2024-10-23 17:57:52 +0200

    Update tibber_tasmota.py

--- tibber_tasmota.py
+++ tibber_tasmota.py
@@ -35,9 +35,11 @@ def main():
 	for i in tibber_response['data']['viewer']['homes'][0]['currentSubscription']['priceInfo']['tomorrow']:	prices[i['startsAt'][0:13]] = i['total']
 	
 	price_avg = 0
+	next_day_future_stop = (datetime.now() + timedelta(days=1)).replace(hour=13, minute=0, second=0, microsecond=0) # don't calculate with the far future
 	future_prices = dict()							# prices to come
 	for i in prices:
-		if datetime.now() < datetime.strptime(i+':59:59', '%Y-%m-%dT%H:%M:%S'):
+		tib_time = datetime.strptime(i+':59:59', '%Y-%m-%dT%H:%M:%S')
+		if datetime.now() < tib_time and tib_time < next_day_future_stop:
 			price_avg += prices[i]
 			future_prices[i] = prices[i]
 	
@@ -45,8 +47,8 @@ def main():
 	price_min = min(future_prices.values())
 	price_max = max(future_prices.values())
 	price_spread = (price_max-price_min)*100
-	price_lt = price_avg - (price_spread / 4 )		# set the divider to your needs
-	price_ut = price_avg #(price_spread / 15 )		# it sets the threshold for the timers
+	price_lt = price_avg #- (price_spread / 2 )		# set the divider to your needs
+	price_ut = price_avg - (price_spread / 9 )		# it sets the threshold
 	price_lt_max = 22								# in ¢, fixed maximum for timer activation
 	
 	if verbose:	print('avg: %.2f'%price_avg,'min: %.2f'%(100*price_min),'max: %.2f'%(100*price_max),'spread: %.2f'%price_spread,'lt: %.2f'%price_lt,'ht: %.2f'%price_ut)
@@ -57,21 +59,26 @@ def main():
 	tib_hour_now = datetime.now().strftime('%Y-%m-%dT%H')
 	timer_is_set = [False]*(len(tasmota_dev)+1) 	# ignore index 0
 	zerotimer = dict()
-	hot_on = True								# True disables hot switching!
+	hot_on = True									# True disables hot switching!
 	hot_off = True
 	
 	for cur_p_time in prices:
 		
 		cur_price = prices[cur_p_time]*100
 		cur_timer = cur_p_time[-2:]+':00'
-		p_hour_is_cur = True if tib_hour_now == cur_p_time else False
-		msg = 'now ' if tib_hour_now == cur_p_time else ' '
-		past_time = False if cur_p_time in future_prices else True
+		calc_time = True if cur_p_time in future_prices else False
 		
-		if past_time:
+		if tib_hour_now == cur_p_time:
+			p_hour_is_cur = True
+			msg = 'now'
+		else:
+			p_hour_is_cur = False
+			msg = ' '
+		
+		if not calc_time:
 			if not verbose: continue
 			p_char = 'o'
-		else:										# current and future hours
+		else:										# current and future hours to calculate
 			
 			if cur_price < price_lt: 				# lower threshold
 				p_char = '>'
@@ -114,7 +121,7 @@ def main():
 				
 				if price_ut < cur_price:			# upper threshold
 					p_char = '<'
-					if not past_time:
+					if calc_time:
 						msg = 'Z '+msg
 						zerotimer[cur_p_time] = cur_price	# current hour will be enabled in the timerfile
 			
@@ -127,7 +134,7 @@ def main():
 		with open('/home/vzlogger/timer.txt','w') as fo:
 			fo.write('# 0000-00-00 for daily repeating, space or tab separated\n#                   battery discharge W if > 100, percentage if <= 100\n# date     time     |   ac inverter power W if > 100, percentage if <= 100\n') #fileheader
 			for tib_form in future_prices:
-				file_form = tib_form.replace('T',' ') + ':00:00 ' + ('100 100' if tib_form in zerotimer else '0 0')
+				file_form = tib_form.replace('T',' ') + ':00:00 ' + ('200 100' if tib_form in zerotimer else '0 0')
 				fo.write(file_form+'\n')
 				if '-vt' in argv : print(file_form)		# console output with -vt
 	
