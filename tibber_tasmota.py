58b3c6ced01bf937103e9a9986c2e78cc4e00420
E-t0m <110770475+E-t0m@users.noreply.github.com>
2024-10-31 10:02:53 +0100

    removed zeroinput support
    see https://github.com/E-t0m/zeroinput/blob/main/tib_zero_tas.py for zeroinput functionality.
    it has tasmota support included.

--- tibber_tasmota.py
+++ tibber_tasmota.py
@@ -12,9 +12,7 @@ tasmota_dev =	{	'auto1_on':{'ip':'192.168.178.88','timer_id': '1','output':'1'},
 					'auto2_off':{'ip':'192.168.178.88','timer_id': '4','output':'2'},
 				}
 
-USE_ZEROINPUT = True
-
-if '-h' in argv or '-help' in argv: print('[ -v verbose ]','[ -vt show timer.txt ]')
+if '-h' in argv or '-help' in argv: print('[ -v verbose ]',' o outside calculation phase, | between the thresholds, > below lower threshold, < above upper threshold')
 
 verbose = True if '-v' in argv else False
 
@@ -39,8 +37,8 @@ def main():
 	for i in tibber_response['data']['viewer']['homes'][0]['currentSubscription']['priceInfo']['tomorrow']:	prices[i['startsAt'][0:13]] = i['total']
 	
 	price_avg = 0
-	next_day_future_stop = (datetime.now() + timedelta(days=1)).replace(hour=13, minute=0, second=0, microsecond=0) # don't calculate with the far future
-	future_prices = dict()							# prices to come
+	next_day_future_stop = (datetime.now() + timedelta(days=1)).replace(hour=13, minute=0, second=0, microsecond=0)	# don't calculate with the time after day time with PV power
+	future_prices = dict()								# prices to come
 	for i in prices:
 		tib_time = datetime.strptime(i+':59:59', '%Y-%m-%dT%H:%M:%S')
 		if datetime.now() < tib_time and tib_time < next_day_future_stop:
@@ -53,7 +51,7 @@ def main():
 	price_spread = (price_max-price_min)*100
 	price_lt = price_avg #- (price_spread * 0.5 )		# lower threshold: set the divider to your needs
 	price_ut = price_avg - (price_spread * 0.1 )		# upper threshold
-	price_lt_max = 27									# in ¢, fixed maximum for timer activation
+	price_lt_max = 25									# in ¢, fixed maximum for timer activation
 	
 	if verbose:	print('avg: %.2f'%price_avg,'min: %.2f'%(100*price_min),'max: %.2f'%(100*price_max),'spread: %.2f'%price_spread,'lt: %.2f'%price_lt,'ht: %.2f'%price_ut)
 	if price_lt > price_lt_max: 
@@ -62,7 +60,6 @@ def main():
 	
 	tib_hour_now = datetime.now().strftime('%Y-%m-%dT%H')
 	timer_is_set = [False]*(len(tasmota_dev)+1) 	# ignore index 0
-	zerotimer = dict()
 	hot_on = True									# True disables hot switching!
 	hot_off = True
 	
@@ -102,7 +99,6 @@ def main():
 						msg += ' 1' if tasmota_timer( tasmota_dev['auto1_on'],cur_timer,'1') == 200 else ' 1FAIL'
 						msg += ' 2' if tasmota_timer( tasmota_dev['auto2_on'],cur_timer,'1') == 200 else ' 2FAIL'
 						if not verbose: syslog.syslog(syslog.LOG_INFO, msg +' at '+ cur_timer)
-				
 			
 			else:									# middle and upper
 				p_char = '|'
@@ -125,22 +121,8 @@ def main():
 				
 				if price_ut < cur_price:			# upper threshold
 					p_char = '<'
-					if calc_time:
-						msg = 'Z '+msg
-						zerotimer[cur_p_time] = cur_price	# current hour will be enabled in the timerfile
-			
 		
 		if verbose: print(str(msg).ljust(20),cur_p_time,'%2.2f'%cur_price,str(p_char).rjust(int(cur_price)))
-	
-	
-	# write a timer file for zeroinput
-	if USE_ZEROINPUT: 
-		with open('/home/vzlogger/timer.txt','w') as fo:
-			fo.write('# 0000-00-00 for daily repeating, space or tab separated\n#                   battery discharge W if > 100, percentage if <= 100\n# date     time     |   ac inverter power W if > 100, percentage if <= 100\n') #fileheader
-			for tib_form in future_prices:
-				file_form = tib_form.replace('T',' ') + ':00:00 ' + ('100 100' if tib_form in zerotimer else '0 0')
-				fo.write(file_form+'\n')
-				if '-vt' in argv : print(file_form)		# console output with -vt
 	
 	return(0)
 
